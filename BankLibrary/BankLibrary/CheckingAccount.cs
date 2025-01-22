dadc807cb8f8de3a0d5635b2c4d6fa26683e6051
bclayson23 <156945138+bclayson23@users.noreply.github.com>
2025-01-21 20:27:40 -0500

    First version of library

---
+++ BankLibrary/BankLibrary/CheckingAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public class CheckingAccount : Account
+    {
+        public decimal OverdraftLimit { get; private set; }
+        public CheckingAccount(string accountNumber, string accountName, decimal initialBalance, decimal overdraftLimit) :
+            base(accountNumber,accountName,initialBalance)
+        {
+            OverdraftLimit = overdraftLimit;
+        }
+
+        public override void Withdraw(decimal amount)
+        {
+            if (amount <= 0) throw new ArgumentException("Withdrawl amount must be positive");
+            if (amount > Balance + OverdraftLimit) throw new ArgumentException("Overdraft Limit exceeded");
+            this.Withdraw(amount);
+        }
+    }
+}
